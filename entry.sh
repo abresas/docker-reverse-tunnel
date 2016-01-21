5dc890e1fce69ad43ebbdd2a4a790516d5db975f
Alexios Theodoros Brezas <abresas@gmail.com>
2016-01-21 16:08:49 +0000

    device ssh port

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
-sshpass -e ssh -N -R 19999:localhost:22 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $MAINTAINANCE_SSH_USER@$MAINTAINANCE_SSH_HOST
+sshpass -e ssh -N -R 19999:localhost:22222 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $MAINTAINANCE_SSH_USER@$MAINTAINANCE_SSH_HOST
