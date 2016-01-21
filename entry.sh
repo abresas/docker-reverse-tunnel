d1c6c75aaa6674ee252ac485523e77486d2a77e0
Alexios Theodoros Brezas <abresas@gmail.com>
2016-01-21 14:10:22 +0000

    diagnose

--- entry.sh
+++ entry.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
+dmesg | tail -n 50
+sleep 10
+free -m
+sleep 10
+journalctl -eu openvpn@client | tail -n 50
+sleep 10
 systemctl status openvpn@client
 sleep 2
 systemctl enable openvpn@client
