87439f078c21b19c3b685d84c5d5c55c7524d817
Alexios Theodoros Brezas <abresas@gmail.com>
2016-01-21 14:41:56 +0000

    maintainance ssh reverse tunnel

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM resin/rpi-raspbian:jessie
 
+RUN apt-get -y update && apt-get install sshpass
+
 RUN mkdir -p /run/systemd/system # systemctl fails without this
 
 COPY ./entry.sh /entry.sh
--- entry.sh
+++ entry.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
-dmesg | tail -n 50
-sleep 10
-free -m
-sleep 10
-journalctl -eu openvpn@client | tail -n 50
-sleep 10
-systemctl status openvpn@client
-sleep 2
-systemctl enable openvpn@client
-sleep 2
-systemctl restart openvpn@client
-sleep 2
-journalctl -u openvpn@client
+sshpass -e ssh -R 19999:localhost:22 $MAINTAINANCE_SSH_USER@$MAINTAINANCE_SSH_HOST
